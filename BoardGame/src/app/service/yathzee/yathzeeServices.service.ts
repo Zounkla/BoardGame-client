e1d7cc20537a60d37d4fc8fd1f9809ff3dbe2cf1
zounkla <flo-vl@orange.fr>
2025-04-28 19:23:02 +0200

    feat: subscribe to SSE to refresh view for all players

--- BoardGame/src/app/service/yathzee/yathzeeServices.service.ts
+++ BoardGame/src/app/service/yathzee/yathzeeServices.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
 import {environment} from '../../../environment/environment';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {BehaviorSubject, Observable, tap} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import { YathzeeGame } from '../../model/yathzee/YathzeeGame.model';
 
 @Injectable({
@@ -11,13 +11,36 @@ export class YathzeeService {
 
   private readonly url: string;
 
-  constructor(private http: HttpClient) {
-    this.url = environment.apiUrl + "yathzee/";
+  constructor(private http: HttpClient, private ngZone: NgZone) {
+    this.url = environment.apiUrl + "yathzee";
   }
+
   getGame(id: string) {
-    const headers = new HttpHeaders({
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
-      });
-    return this.http.get<YathzeeGame>(this.url + id, {headers});
+    return this.http.get<YathzeeGame>(this.url + "/" + id);
   }
+
+  subscribeToGameUpdates(id: string): Observable<YathzeeGame> {
+    const token = localStorage.getItem('authToken');
+    return new Observable<YathzeeGame>((observer) => {
+      const eventSource = new EventSource(`${this.url}/${id}/stream?token=${token}`);
+
+      eventSource.addEventListener('game-update', (event: MessageEvent) => {
+        this.ngZone.run(() => {
+          const updatedGame: YathzeeGame = JSON.parse(event.data);
+          observer.next(updatedGame);
+        });
+      });
+
+      eventSource.onerror = (error) => {
+        console.error('SSE error: ', error);
+        eventSource.close();
+        observer.error(error);
+      };
+
+      return () => {
+        eventSource.close();
+      };
+    });
+  }
+
 }
--- BoardGame/src/app/yathzee/yathzee-game/yathzee-game.component.ts
+++ BoardGame/src/app/yathzee/yathzee-game/yathzee-game.component.ts
@@ -1,21 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import { YathzeeDicesComponent } from '../yathzee-dices/yathzee-dices.component';
 import { YathzeeGame } from '../../model/yathzee/YathzeeGame.model';
 import { YathzeeService } from '../../service/yathzee/yathzeeServices.service';
 import { Router } from '@angular/router';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-yathzee-game',
   imports: [YathzeeDicesComponent],
   templateUrl: './yathzee-game.component.html',
   styleUrl: './yathzee-game.component.scss'
 })
-export class YathzeeGameComponent implements OnInit {
+export class YathzeeGameComponent implements OnInit, OnDestroy {
   @Input() id: string = "";
   yathzeeGame: YathzeeGame | null = null;
+  private gameUpdatesSubscription: Subscription | null = null;
 
   constructor(private service: YathzeeService, private router: Router){}
 
   ngOnInit(): void {
-    this.service.getGame(this.id).subscribe(data => this.yathzeeGame = data);
+    this.service.getGame(this.id).subscribe({
+      next: (data) => {
+        this.yathzeeGame = data;
+        this.listenForGameUpdates();
+      },
+      error: (err) => {
+        console.error('Error loading game', err);
+      }
+    });
+  }
+
+  listenForGameUpdates() {
+    this.gameUpdatesSubscription = this.service.subscribeToGameUpdates(this.id).subscribe({
+      next: (updatedGame) => {
+        this.yathzeeGame = updatedGame;
+      },
+      error: (err) => {
+        console.error('Error receiving game updates', err);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameUpdatesSubscription) {
+      this.gameUpdatesSubscription.unsubscribe();
+    }
   }
 }
